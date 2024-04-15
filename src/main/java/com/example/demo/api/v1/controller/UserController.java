518fd6ff67ded48734284e90ea6c8a9cd78be4a5
Takashi Yamashina <takashi.yamashina@gmail.com>
2024-04-15 23:44:43 +0900

    Remove unused import statement

--- src/main/java/com/example/demo/api/v1/controller/UserController.java
+++ src/main/java/com/example/demo/api/v1/controller/UserController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
